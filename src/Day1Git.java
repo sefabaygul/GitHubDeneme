fdd4b90158adb7d89e1f040d96aba8854482fc8e
Sefa Baygül <sefa.baygul06@gmail.com>
2022-08-13 22:59:06 +0300

    6.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,5 +2,7 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. deneme");
         System.out.println("Hello Git 2. deneme");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git 4.");
     }
 }
