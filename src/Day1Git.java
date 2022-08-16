f18cdbc37207d7a022a21832fc8a9f3cbb2c9343
sefabaygul <110894002+sefabaygul@users.noreply.github.com>
2022-08-16 22:09:11 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -5,5 +5,6 @@ public class Day1Git {
         System.out.println("Hello Git");
         System.out.println("Hello Git 4.");
         System.out.println("Hello Git 5.");
+        System.out.println("Hello Git 6.");
     }
 }
